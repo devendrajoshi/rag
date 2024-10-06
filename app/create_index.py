47cce152469219c2e8fb7ee26528d5865ae663e5
Devendra Joshi <devendra.joshi@agilent.com>
2024-10-06 12:49:28 -0700

    FASTAPI code

---
+++ app/create_index.py
@@ -0,0 +1,111 @@
+import os
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+import PyPDF2
+from langchain_chroma import Chroma
+from langchain_core.documents import Document
+from langchain_huggingface import HuggingFaceEmbeddings
+from dotenv import load_dotenv
+import boto3
+from datetime import datetime
+import requests
+
+
+load_dotenv()
+
+# Load environment variables
+INDEX_PATH = os.getenv('INDEX_PATH', './index/')
+EMBEDDING_MODEL_NAME = os.getenv('EMBEDDING_MODEL_NAME', 'sentence-transformers/all-mpnet-base-v2')
+LOCAL_DOCS_PATH = os.getenv('LOCAL_DOCS_PATH', './localdocs/')
+#LOCAL_DOCS_PATH = os.getenv('LOCAL_DOCS_PATH', 's3://dataseek.dev.net/rag_docs/')
+SPLITTER_CHUNK_SIZE = int(os.getenv('SPLITTER_CHUNK_SIZE', 1000))
+SPLITTER_CHUNK_OVERLAP = int(os.getenv('SPLITTER_CHUNK_OVERLAP', 200))
+
+splitter = RecursiveCharacterTextSplitter(chunk_size=SPLITTER_CHUNK_SIZE, chunk_overlap=SPLITTER_CHUNK_OVERLAP)
+
+
+def get_vector_db():
+    requests.packages.urllib3.disable_warnings()
+    embeddings = HuggingFaceEmbeddings(model_name=EMBEDDING_MODEL_NAME)
+    vector_db = Chroma(
+        collection_name="local_collection",
+        embedding_function=embeddings,
+        persist_directory=INDEX_PATH+"embeddings_db",  # Where to save data locally, remove if not neccesary
+    )
+    return vector_db
+
+
+def create_index():
+    current_time = datetime.now()
+    vector_db = get_vector_db()
+    #empty the vector_db before adding new documents
+    vector_db.reset_collection()
+    doc_cnt = 0
+    #if the LOCAL_DOCS_PATH is local file path
+    if os.path.exists(LOCAL_DOCS_PATH) and os.listdir(LOCAL_DOCS_PATH):
+        doc_cnt = read_local_files(vector_db)
+    
+    elif LOCAL_DOCS_PATH.startswith('s3://'):
+        doc_cnt = read_s3_files(vector_db)
+
+    else:
+        print("Invalid LOCAL_DOCS_PATH or empty directory: ", LOCAL_DOCS_PATH)
+
+    print(f"Indexed {doc_cnt} documents in {datetime.now() - current_time}")
+
+def process_pdf(file, source, doc_cnt):
+    global splitter
+    documents = []
+    id_cnt = doc_cnt
+    reader = PyPDF2.PdfReader(file)
+    text = ''
+    page_cnt = 0
+    for page in range(len(reader.pages)):
+        text = reader.pages[page].extract_text()
+        page_cnt += 1
+        for chunk in splitter.split_text(text):
+            id_cnt += 1
+            doc = Document(
+                page_content=chunk,
+                metadata={"source": source, "page": page_cnt},
+                id=id_cnt,
+            )
+            documents.append(doc)
+    print(f"Read {page_cnt} pages")
+    return documents
+
+def read_local_files(vector_db):
+    doc_cnt = 0
+    for filename in os.listdir(LOCAL_DOCS_PATH):
+        if filename.endswith('.pdf'):
+            with open(os.path.join(LOCAL_DOCS_PATH, filename), 'rb') as file:
+                print(f"Reading {filename}")
+                docs = process_pdf(file, filename, doc_cnt)
+                doc_cnt += len(docs)
+                vector_db.add_documents(documents=docs)
+    return doc_cnt
+
+def read_s3_files(vector_db):
+    doc_cnt = 0
+    s3 = boto3.client('s3')
+    # Extract bucket name and s3_docs_path from LOCAL_DOCS_PATH
+    path_parts = LOCAL_DOCS_PATH.replace("s3://", "").split("/", 1)
+    bucket_name = path_parts[0]
+    s3_docs_path = path_parts[1] if len(path_parts) > 1 else ""
+    
+    # List all objects in the specified S3 path
+    response = s3.list_objects_v2(Bucket=bucket_name, Prefix=s3_docs_path)
+    
+    for obj in response.get('Contents', []):
+        key = obj['Key']
+        if key.endswith('.pdf'):
+            print(f"Reading {key}")
+            
+            # Download the file from S3
+            s3.download_file(bucket_name, key, '/tmp/temp.pdf')
+            
+            with open('/tmp/temp.pdf', 'rb') as file:
+                docs = process_pdf(file, key, doc_cnt)
+                doc_cnt += len(docs)
+                vector_db.add_documents(documents=docs)
+    
+    return doc_cnt
---
+++ app/main.py
@@ -0,0 +1,177 @@
+from fastapi import FastAPI, HTTPException, BackgroundTasks, Request, Response
+from pydantic import BaseModel, Field
+from langchain_ollama.llms import OllamaLLM
+from sentence_transformers import SentenceTransformer
+from langchain_chroma import Chroma
+from create_index import create_index, get_vector_db, LOCAL_DOCS_PATH, SPLITTER_CHUNK_SIZE, SPLITTER_CHUNK_OVERLAP
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.prompts import PromptTemplate
+from typing import Optional
+import requests
+import traceback
+from fastapi.middleware.gzip import GZipMiddleware
+import io
+import os
+from dotenv import load_dotenv
+import ssl
+
+os.environ["CURL_CA_BUNDLE"] = "" # Disable SSL verification
+
+# Load environment variables
+INDEX_PATH = os.getenv('INDEX_PATH', './index/')
+LLM_MODEL = os.getenv('LLM_MODEL', 'llama3.2:1b')
+LLM_HOST = os.getenv('LLM_HOST', 'ollama')
+LLM_PORT = int(os.getenv('LLM_PORT', '11434'))
+RAG_PROMPT_TEMPLATE_TXT = os.getenv('RAG_PROMPT_TEMPLATE_TXT', "You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. If you don't know the answer, just say that you don't know. Use five sentences maximum and keep the answer concise.")
+
+ollama_base_url = f"http://{LLM_HOST}:{LLM_PORT}"
+
+rag_prompt_template = PromptTemplate.from_template(RAG_PROMPT_TEMPLATE_TXT+"""
+                        Question: {question} 
+                        Context: {context} 
+                        Answer:""")
+
+class RequestModel(BaseModel):
+    #prompt is mandatory
+    prompt: str
+    session_context: Optional[str] = Field(None)
+
+def check_and_download_model():
+    # Check if the model is available
+    response = requests.get(f"{ollama_base_url}/models")
+    if response.status_code == 200:
+        models = response.json()
+        if LLM_MODEL in models:
+            print(f"Model {LLM_MODEL} is already available.")
+            return
+    # Download the model
+    print(f"Downloading model {LLM_MODEL}...")
+    response = requests.post(f"{ollama_base_url}/api/pull", json={"name": LLM_MODEL})
+    if response.status_code == 200:
+        print(f"Model {LLM_MODEL} downloaded successfully.")
+    else:
+        print(f"Failed to download model {LLM_MODEL}. Status code: {response.status_code}")
+    
+
+app = FastAPI(
+    title = "RAG Tutorial",
+    version ="1.0",
+    description ="A simple API Server"
+)
+
+app.add_middleware(GZipMiddleware, minimum_size=1000, compresslevel=1)
+
+
+check_and_download_model()
+
+vector_db = None
+is_index_creation_running = False
+
+
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+def load_vector_db():
+    global vector_db
+    vector_db = get_vector_db()
+
+# if index folder exist and is not empty then load the vector db
+if os.path.exists(INDEX_PATH) and os.listdir(INDEX_PATH):
+    load_vector_db()
+
+#create base_url using the host and port
+
+llm = OllamaLLM(model=LLM_MODEL, base_url=ollama_base_url)
+
+
+#craete a endpoint to check all the environment variables
+@app.get("/debug/")
+async def get_env():
+    data = {"INDEX_PATH":INDEX_PATH,
+            "LLM_MODEL":LLM_MODEL,
+            "LLM_HOST":LLM_HOST,
+            "LLM_PORT":LLM_PORT,
+            "RAG_PROMPT_TEMPLATE_TXT":RAG_PROMPT_TEMPLATE_TXT,
+            "SPLITTER_CHUNK_SIZE":SPLITTER_CHUNK_SIZE,
+            "SPLITTER_CHUNK_OVERLAP":SPLITTER_CHUNK_OVERLAP,
+            "LOCAL_DOCS_PATH":LOCAL_DOCS_PATH}
+    
+    try:
+        res = requests.get(f"http://{LLM_HOST}:{LLM_PORT}")
+        data["LLM_SERVER_STATUS"] = res.status_code
+        res = requests.get(f"http://{LLM_HOST}:{LLM_PORT}/api/tags")
+        data["LLM_MODELS"] =  res.json()
+    except Exception as e:
+        data["LLM_SERVER_STATUS"] = str(e)
+    return data
+
+def create_index_background():
+    create_index()
+    #check if the index is created
+    if os.path.exists(INDEX_PATH) and os.listdir(INDEX_PATH):
+        load_vector_db()
+        print("Index created successfully")
+    else:
+        print("Index creation failed")
+
+#create a endpoint to create the index
+@app.post("/create_index/")
+async def create_index_endpoint(background_tasks: BackgroundTasks):
+    global is_index_creation_running
+    if is_index_creation_running:
+        return {"message":"Index creation is already running"}
+    else:
+        is_index_creation_running = True
+        background_tasks.add_task(create_index_background)
+        return {"message":"Index creation started in background"}  
+
+
+@app.post("/query/")
+async def generate_response(request: RequestModel):
+    session_context = ""
+    if request.session_context:
+        session_context = request.session_context
+    if len(session_context) > 0:
+        local_prompt = session_context + "\n" +request.prompt
+    else:
+        local_prompt = request.prompt
+    try:
+        prompts = []
+        prompts.append(local_prompt)
+        #if retriever is None then generate the response without it
+        if vector_db is None:
+            return llm.generate(prompts)
+        else:
+            retriever = vector_db.as_retriever()
+            docs = retriever.invoke(local_prompt)
+            # Check if there are hits in the vector_db
+            if not docs:
+                return llm.generate(prompts)
+            
+            prompt_template = rag_prompt_template
+            if request.prompt_template:
+                prompt_template = PromptTemplate.from_template(request.prompt_template+"""
+                                                                Question: {question}
+                                                                Context: {context}
+                                                                Answer:""")
+            rag_chain = (
+                {"context": retriever | format_docs, "question": RunnablePassthrough()}
+                | prompt_template
+                | llm
+                | StrOutputParser()
+            )
+            
+            res = rag_chain.invoke(local_prompt)
+            output = {"response":res,
+                    "session_context":session_context}
+            return output
+    except Exception as e :
+        print(str(e))
+        traceback.print_exc()
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+
+
+
